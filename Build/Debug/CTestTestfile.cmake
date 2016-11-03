c809f3a36e9deaa25ce245f5cb1e91dad3af09c1
laila <laila@fishbot-Precision-WorkStation-T3500>
2016-11-03 15:13:29 +0100

    Stop following the Build folder.

--- Build/Debug/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/Macbookair/Desktop/MT/MA1/Projet de Semestre I/FishSwarm
-# Build directory: /Users/Macbookair/Desktop/MT/MA1/Projet de Semestre I/FishSwarm/Build/Debug
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
